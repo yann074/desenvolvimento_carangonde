146430225a7dc8a34a4f6eae514b1d9338f32de6
yann074 <yannsmith2907@gmail.com>
2024-04-21 02:56:51 -0300

    update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Loginl.css'
 
 export const Login = () => {
   return (
     <>
-    <h1>OLÁ MUNDO</h1>
+    <section className='container_login'>
+        <div className='container_login_apresentation'>
+            <h1>Bem Vindo De Volta</h1>
+            <h3>Entre Para Acessar Nosso Conteúdo</h3>
+        </div>
+        <div className='container_login_op'>
+            <h2>Login</h2>
+            <button className='google'>GOOGLE</button>
+            <input type="email" placeholder='Digite seu email' />
+            <input type="password" placeholder='Digite sua senha'/>
+            <button className='entrar'>ENTRAR</button>
+            <h5>Esqueci Minha Senha</h5>
+            <h5>NÃO TEM CONTA? CADASTRE-SE AQUI</h5>
+        </div>
+    </section>
     </>
   )
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Registerl.css'
+
+export const Register = () => {
+  return (
+    <>
+    <section className='container_register'>
+        <div className='container_register_apresentation'>
+            <h1>Crie Sua Conta</h1>
+            <h3>Junte-se à nossa comunidade e Tenha Acesso aos nossos cursos.</h3>
+        </div>
+        <div className='container_register_op'>
+            <h2>CADASTRE-SE</h2>
+            <button>GOOGLE</button>
+            <input type="text" placeholder='Digite seu nome completo'/>
+            <input type="email" placeholder='Digite seu email' />
+            <input type="password" placeholder='Digite sua senha'/>
+            <input type="password" placeholder='Confirme sua senha' />
+            <input type="text" placeholder='Digite seu CPF' />
+            <label htmlFor="">Data de Nascimento</label>
+            <input type="date" />
+            <button>CADASTRAR</button>
+        </div>
+    </section>
+    </>
+  )
+}
