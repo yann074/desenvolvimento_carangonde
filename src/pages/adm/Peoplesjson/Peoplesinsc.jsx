280755deb95bc2f8729fb2fc61631c272450f938
yann074 <yannsmith2907@gmail.com>
2024-06-10 11:00:09 -0300

    atualização: tela de inscritos

--- src/pages/adm/Peoplesjson/Peoplesinsc.jsx
+++ src/pages/adm/Peoplesjson/Peoplesinsc.jsx
@@ -6,35 +6,46 @@ export const Peoplesinsc = () => {
 
 
     useEffect(()=>{
-      fetch('http://localhost:5173/src/pages/adm/Peoplesjson/peoples.json')
-        .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
+      //fetch('http://localhost:5173/src/pages/adm/Peoplesjson/peoples.json')
+      fetch("http://localhost/backend/public/adm.php")
+      .then(response => {
+        console.log(response.data);
       })
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
-    }, [])
+      .then(data => {
+        if (data.message === 'Dados recebidos com sucesso') {
+          setData(data.data);
+        } else {
+          console.error('Nenhum usuário encontrado');
+        }
+      })
+      .catch(error => console.error('Erro ao buscar dados:', error));
+  }, []);
 
   return (
     <>
-    <section>
-        <table  className='container-people'>
-          {data.map(item=>{
-            const  {name, cursos, tell, email, cpf} = item
-            return(
-              <tr className='grid-p'>
-                <th>{name}</th>
-                <th>{cursos}</th>
-                <th>{tell}</th>
-                <th>{email}</th>
-                <th>{cpf}</th>
+   <section>
+      {data.length > 0 ? (
+        <table className='container-people'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>CPF</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index} className='grid-p'>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.cpf}</td>
               </tr>
-            )
-          })}
-        
+            ))}
+          </tbody>
         </table>
+      ) : (
+        <p>Nenhum usuário encontrado</p>
+      )}
     </section>
     </>
   )
